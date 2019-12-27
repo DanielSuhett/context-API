5b779ab25bcf9130b9394ccda784884f39c45629
DanielSuhett <danysuhett@hotmail.com>
2019-12-27 13:45:39 -0300

    Updates

---
+++ precache-manifest.b5ded523b4491cd943e7843ce701207b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b6daecee5a5d9284fe744837822f35",
+    "url": "/context-API/index.html"
+  },
+  {
+    "revision": "9ffa2ee77a8ae1bcd4fb",
+    "url": "/context-API/static/css/main.2312c3b3.chunk.css"
+  },
+  {
+    "revision": "227d654a503190678f84",
+    "url": "/context-API/static/js/2.48988a1d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/context-API/static/js/2.48988a1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ffa2ee77a8ae1bcd4fb",
+    "url": "/context-API/static/js/main.f629305e.chunk.js"
+  },
+  {
+    "revision": "633f5f19d912756f61ba",
+    "url": "/context-API/static/js/runtime-main.0af94fdb.js"
+  }
+]);
